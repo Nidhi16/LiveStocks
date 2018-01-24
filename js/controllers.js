c3c2b30a34508269988d929a5a8f51bc970bd9c1
Nidhi16 <nidhi.valuable@gmail.com>
2018-01-25 01:58:46 +0530

    Added a hack to reflect the changes in the scope by calling $scope.$digest() every second

--- js/controllers.js
+++ js/controllers.js
@@ -1,5 +1,10 @@
-app.controller('StocksController', function($scope, stocksFactory) {
+app.controller('StocksController', function($scope, StocksFactory) {
 
-    $scope.stocks = stocksFactory.getStocks();
+    $scope.stocks = StocksFactory.stocksCurrentPrices;
 
+    // HACK This is used to force the digest every second as the changes in $scope.stocks
+    // was not reflected in the UI
+    setInterval(function() {
+        $scope.$digest();
+    }, 1000);
 });
--- js/factories.js
+++ js/factories.js
@@ -1,23 +1,45 @@
-app.factory('stocksFactory', function() {
+app.factory('StocksFactory', function() {
+    var stocksData = {};
     var factory ={};
+    factory.stocksCurrentPrices = {};
 
-    var stocks = {
-        'goog': {
-            'price': 100,
-            'time': new Date().toISOString()
-        },
-        'msft': {
-            'price': 200,
-            'time': new Date().toISOString()
-        },
-        'intl': {
-            'price': 300,
-            'time': new Date().toISOString()
-        }
-    };
+    // Local copy of the stock current prices
+    var stocksCurrentPrices = {};
 
-    factory.getStocks = function() {
-        return stocks;
+    const stocksSocket = new WebSocket('ws://stocks.mnet.website');
+    stocksSocket.onmessage = function(event) {
+        var tickerTime = new Date();  // // Time of the data update
+        var data = JSON.parse(event.data);
+
+        data.forEach(function (value) {
+            var tickerId = value[0];  // Ticker ID
+            var tickerPrice = value[1]; // Ticker Price
+
+            var change = -1;
+            if (!(tickerId in stocksData)) {
+                stocksData[tickerId] = [];
+                stocksCurrentPrices[tickerId] = {};
+                change = 0;
+            }
+            if (change !== 0) {
+                // Comparing to previous price
+                var tickerPrices = stocksData[tickerId];
+                var previousPrice = tickerPrices[tickerPrices.length - 1]['price'];
+                change = previousPrice > tickerPrice ? 2 : 1;
+            }
+            var thisStockData = {
+                'price': tickerPrice,
+                'time': tickerTime,
+                'change': change
+            };
+            // Adding for history
+            stocksData[tickerId].push(thisStockData);
+            // Adding to current prices
+            stocksCurrentPrices[tickerId] = thisStockData;
+
+            // Live update the values in the factory
+            angular.copy(stocksCurrentPrices, factory.stocksCurrentPrices);
+        });
     };
 
     return factory;
