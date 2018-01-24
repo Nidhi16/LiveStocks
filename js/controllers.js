62e1a4ff83c6d8b7dd86eff44fe0fc64d1d41238
Nidhi16 <nidhi.valuable@gmail.com>
2018-01-25 00:16:53 +0530

    Displaying hardcoded stocks

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,16 @@
 app.controller('StocksController', function($scope) {
-    $scope.greetings = 'Hello Angular!';
+    $scope.stocks = {
+        'goog': {
+            'price': 100,
+            'time': new Date().toISOString()
+        },
+        'msft': {
+            'price': 200,
+            'time': new Date().toISOString()
+        },
+        'intl': {
+            'price': 300,
+            'time': new Date().toISOString()
+        }
+    };
 });
